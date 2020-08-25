77b354d9c0aa7c9dc77f801b292f7898d62c3dcb
at89s52rc <1312788508@qq.com>
2020-08-25 17:27:38 +0800

    Update test01.js

--- test01.js
+++ test01.js
@@ -2,15 +2,24 @@
 
 //氧气听书
 
-console.log('测试写出日志'); //输出日志
+//console.log('测试写出日志'); //输出日志
 
-$notification.post('title标题', 'subTitle子标题子标题子标题','body内容内容内容内容') //用于通知栏提醒
-
-
+//$notification.post('title标题', 'subTitle子标题子标题子标题','body内容内容内容内容') //用于通知栏提醒
 
 
 
-$done();
+
+var body = $response.body; // 声明一个变量body并以响应消息体赋值
+var obj = JSON.parse(body); // JSON.parse()将json形式的body转变成对象处理
+
+
+obj.userInfo.isMember=true;//修改为true
+obj.userInfo.startDate = "2020-08-26 12:30:45";
+obj.userInfo.endDate = "2030-08-30 12:30:45";
+
+
+body = JSON.stringify(obj); // 重新打包回json字符串
+$done({body}); // 结束修改
 
 
 
